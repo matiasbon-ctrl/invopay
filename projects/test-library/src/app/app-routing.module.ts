a390bdc89a643553dc8bad00453ad9464c471a40
matias406042 <142633399+matias406042@users.noreply.github.com>
2025-10-23 13:10:42 -0300

    fix(guard-routes): coverin all private routes

--- projects/test-library/src/app/app-routing.module.ts
+++ projects/test-library/src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
   ,
    {
     path: '', 
+    canActivate:[AuthPrivateGuard],
     component: HomeComponent,
     children: [
       { path: 'sales-list', component: SalesListComponent ,canActivate: [AuthPrivateGuard] },
